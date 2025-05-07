40a3e5138e670085626688b2dcbfd3a8c72d6d05
Sohaib-2310 <muhammadsohaib8008@gmail.com>
2025-05-08 02:30:09 +0500

    Services page updated - Added our client section in services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,6 +4,8 @@ import Button from '../components/Button'
 import HomeIcon from '../assets/s1.svg'
 import OfficeIcon from '../assets/s2.svg'
 import BodyguardIcon from '../assets/s3.svg'
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
+import clientImg from '../assets/client.jpeg'
 
 const Services = () => {
     return (
@@ -39,7 +41,27 @@ const Services = () => {
                             excepturi ullam.</p>
                         <Button title='Read More' />
                     </div>
+                </div>
+            </div>
 
+            <div className="ourClient">
+                <div className="slideBtn left">
+                    <FaCircleChevronLeft />
+                </div>
+
+                <div className="clientContainer">
+                    <h1>What is says our clients</h1>
+                    <div className="clientInfo">
+                        <img src={clientImg} alt="" />
+                        <p className="clientName">Muhammad Sohaib</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                </div>
+
+                <div className="slideBtn left">
+                    <FaCircleChevronRight />
                 </div>
             </div>
         </>
